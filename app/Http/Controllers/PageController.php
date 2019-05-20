0a85ca64da41db7813500f442c8504a82b149348
QuyenTran123 <quyen.tran@student.passerellesumeriques.org>
2019-05-21 00:12:17 +0700

    dang bai

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Input;
 use App\Product;
 use App\Post_Product;
 use App\Category;
@@ -34,13 +35,48 @@ class PageController extends Controller
     	return view("customer.Page.baidang");
     }
 
-    public function getLoaiSp($type) {
-        $sp_theoloai= Product::where('id_type',$type) ->limit(3)->get();
+   
+
+      public function insertProduct(){
+        $product = new Product;
+        $product->name = Input::get('name');
+        $product->id = Input::get(2);
+        $product->price = Input::get('price');
+        $product->description = Input::get('description');
+        $product->image =  Input::get('image');
+        $product->status =  Input::get(1);
+        $product->save();
+
+        return Redirect::back();
+    }
+
+    public function insertPost(){
+        $product = new Post;
+        $product->id_user = Input::get('name');
+        $product->price = Input::get('price');
+        $product->description = Input::get('description');
+        $product->image =  Input::get('image');
+        $product->status =  Input::get(1);
+        $product->save();
+
+        return Redirect::back();
+    }
+
+
+
+    public function loadCate()
+    {
+        $name = Category::select('id','name')->get();
+        return view('customer.Page.baidang',compact('name'));
+    }
+
+   // public function getLoaiSp($type) {
+      //  $sp_theoloai= Product::where('id_type',$type) ->limit(3)->get();
         // $sp_khac= Product::where('id_type','<>',$type)->limit(3)->get();
-        $loai = Category::all();
-        $loai_sp = Category::where('id',$type)->first();
-        return view('Customer.Page.baidang',compact('sp_theoloai','loai','loai_sp'));
-    
+       // $loai = Category::all();
+       // $loai_sp = Category::where('id',$type)->first();
+       // return view('Customer.Page.baidang',compact('sp_theoloai','loai','loai_sp'));
+   // }
 
     function gettuthien(){
         return View("Customer.Page.tuthien");
